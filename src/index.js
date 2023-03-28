a4a8e5ef31d535f0ea341743f514b809b035a214
Harriet Oteng <harriet.oteng@gmail.com>
2023-03-28 23:25:49 +0000

    Add tasks

--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@ import _ from 'lodash';
 import './style.css';
 
 const tasks = [
-  {description: 'wash the dishes', completed: false, index: 0},
-  {description: 'complete To Do list project', completed: false, index: 1},
-  {description: 'finish python exercises', completed: true, index: 0}
-]
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 0
+  },
+  {
+    description: 'complete To Do list project',
+    completed: false,
+    index: 1
+  },
+  {description:
+    'finish python exercises',
+    completed: true,
+    index: 2
+  },
+];
+
+// get ul from index.html
+const taskContainer = document.getElementById('list-items');
+
+tasks.forEach((task) => {
+  const taskItem = document.createElement('li');
+  taskItem.classList.add('todo');
+  taskItem.innerHTML = `
+  <button class="checkbox"></button>
+  <p class="">${task.description}</p>
+  <div class="dots">
+    <span class="material-icons">more_vert</span>
+  </div>
+  `;
+  taskContainer.appendChild(taskItem);
+});
