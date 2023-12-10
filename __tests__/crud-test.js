3416cead49364e2a749255fa209d24fd3d3b0b4d
Harriet Oteng <harriet.oteng@gmail.com>
2023-12-10 11:42:19 +0000

    Add test for display Tasks function

--- __tests__/crud-test.js
+++ __tests__/crud-test.js
@@ -3,10 +3,7 @@
  */
 
 import Task from '../src/modules/task.js';
-import { addTaskToStorage, addTaskToDOM } from '../src/modules/crud.js';
-
-// import { addTaskToStorage, addTaskToDOM, displayTasks, removeTask, editTask } from './src/modules/crud.js';
-// const { addTaskToStorage, addTaskToDOM, displayTasks, removeTask, editTask } = require('./src/modules/crud.js');
+import { addTaskToStorage, addTaskToDOM, displayTasks } from '../src/modules/crud.js';
 
 describe('Task class', () => {
   test('creates a new task object', () => {
@@ -35,16 +32,39 @@ describe('addTaskToStorage', () => {
 describe('addTaskToDOM', () => {
   test('adds task description to the DOM', () => {
     const task = new Task('code', 2);
-    document.body.innerHTML =
-      '<div>' +
-      '  <ul id="list-items"></ul>' +
-      '</div>';
-    
+    document.body.innerHTML = '<div>'
+      + '  <ul id="list-items"></ul>'
+      + '</div>';
+
     const container = document.getElementById('list-items');
 
     addTaskToDOM(task, container);
     const desc = document.querySelector('.desc');
 
-    expect(desc.innerHTML).toBe('code')
-  })
+    expect(desc.innerHTML).toBe('code');
+  });
+});
+
+describe('displayTasks', () => {
+  test('displays an array of task object in the DOM', () => {
+    const taskArray = [
+      { description: 'wash', completed: false, index: 1 },
+      { description: 'code', completed: true, index: 2 },
+      { description: 'call mum', completed: false, index: 3 },
+    ];
+
+    document.body.innerHTML = '<div>'
+      + '  <ul id="list-items"></ul>'
+      + '</div>';
+
+    const container = document.getElementById('list-items');
+
+    expect(document.querySelectorAll('.todo').length).toBe(0);
+
+    // Act
+    displayTasks(taskArray, container);
+
+    const listItems = document.querySelectorAll('.todo');
+    expect(listItems.length).toBe(3);
+  });
 });
